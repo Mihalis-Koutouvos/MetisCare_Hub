f732859301827f2e827ff84e89bf6272fd45bea7
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2024-12-18 16:20:54 -0500

    Added attributes to the general ward table

--- database/metiscare_hub_db.sql
+++ database/metiscare_hub_db.sql
@@ -204,6 +204,7 @@ CREATE TABLE private_rooms (
     storedSnacks BOOLEAN NOT NULL,
     personalHygiene BOOLEAN NOT NULL,
     portableOxygen BOOLEAN NOT NULL,
+    biohazardBags BOOLEAN NOT NULL,
     FOREIGN KEY (id) REFERENCES rooms (id)
 );
 
@@ -238,6 +239,7 @@ CREATE TABLE semi_private_rooms (
     storedSnacks BOOLEAN NOT NULL,
     personalHygiene BOOLEAN NOT NULL,
     portableOxygen BOOLEAN NOT NULL,
+    biohazardBags BOOLEAN NOT NULL,
     FOREIGN KEY (id) REFERENCES rooms (id)
 );
 
@@ -277,6 +279,7 @@ CREATE TABLE intensive_care_unit_rooms (
     overbedTables BOOLEAN NOT NULL,
     mobilityAids BOOLEAN NOT NULL,
     dialysisMachines BOOLEAN NOT NULL,
+    biohazardBags BOOLEAN NOT NULL,
     FOREIGN KEY (id) REFERENCES rooms (id)
 );
 
@@ -309,6 +312,7 @@ CREATE TABLE emergency_room_rooms (
     gauzePads BOOLEAN NOT NULL, 
     scissors BOOLEAN NOT NULL, 
     splints BOOLEAN NOT NULL, 
+    biohazardBags BOOLEAN NOT NULL,
     FOREIGN KEY (id) REFERENCES rooms (id)
 );
 
@@ -349,6 +353,7 @@ CREATE TABLE operating_rooms (
     suctionDevices BOOLEAN NOT NULL,
     esu BOOLEAN NOT NULL,
     implants BOOLEAN NOT NULL,
+    biohazardBags BOOLEAN NOT NULL,
     FOREIGN KEY (id) REFERENCES rooms (id)
 );
 
@@ -397,7 +402,34 @@ CREATE TABLE step_down_unit_rooms (
 -- Create general_ward_rooms table --
 CREATE TABLE general_ward_rooms (
     id INT PRIMARY KEY,
-
+    besideMonitors BOOLEAN NOT NULL,
+    centralMonitoringSystems BOOLEAN NOT NULL,
+    icpMonitors BOOLEAN NOT NULL,
+    defibrillator BOOLEAN NOT NULL,
+    feedingPumps BOOLEAN NOT NULL,
+    overbedTables BOOLEAN NOT NULL,
+    mobilityAids BOOLEAN NOT NULL,
+    ventillator BOOLEAN NOT NULL,
+    ivFluid BOOLEAN NOT NULL,
+    painRelievers BOOLEAN NOT NULL,
+    sedatives BOOLEAN NOT NULL,
+    emegencyDrugs BOOLEAN NOT NULL,
+    testingKits BOOLEAN NOT NULL,
+    thermometers BOOLEAN NOT NULL,
+    oxygenTanks BOOLEAN NOT NULL,
+    ivCatheters BOOLEAN NOT NULL,
+    bvm BOOLEAN NOT NULL, 
+    bandages BOOLEAN NOT NULL,
+    alcoholSwabs BOOLEAN NOT NULL,
+    gauzePads BOOLEAN NOT NULL, 
+    scissors BOOLEAN NOT NULL, 
+    sharpsContainer BOOLEAN NOT NULL,
+    stethoscopes BOOLEAN NOT NULL,
+    hydrations BOOLEAN NOT NULL,
+    storedSnacks BOOLEAN NOT NULL,
+    personalHygiene BOOLEAN NOT NULL,
+    portableOxygen BOOLEAN NOT NULL,
+    biohazardBags BOOLEAN NOT NULL,
     FOREIGN KEY (id) REFERENCES rooms (id)
 );
 
