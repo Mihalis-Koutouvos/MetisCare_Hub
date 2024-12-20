f4a4e5cede7a38c8505284fe0bd2e3344b95d637
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2024-12-20 14:52:13 -0500

    Moved around one of the imports in App.js

--- frontend_app/src/App.js
+++ frontend_app/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './Navbar';
 import Home from './Home';
 import Rooms from './Rooms';
 import About from './About';
 import Doctors from './Doctors';
 import Contact from './Contact';
-import Navbar from './Navbar';
 
 function App() {
   return (
