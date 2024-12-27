74c5daf7ea26c87355a392796777bde5d35b5807
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2024-12-27 15:04:54 -0500

    Added styles to outputting lists functionality.

--- frontend_app/src/Home.js
+++ frontend_app/src/Home.js
@@ -1,31 +1,28 @@
 import {useState} from 'react';
 
 const Home = () => {
-
-    //let name = 'Mario';
-    //We cannot simply just change the text from a variable outside a function in a function. What
-    //we have to do is make the variable reactive by using useState, which takes in whatever we 
-    //are passing in and a function associated with your necessary operation. We are essentially
-    //re-rendering. The process we are doing with the array is called array destructuring. If 
-    //you need something to change at some point, use a State Hook.
-    const [name, setName] = useState('Mario');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => {
-        setName('Luigi');
-        setAge(30);
-    }
-
+    //Initial value is array of blogs in useState
+    //Each id needs to be unique
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'hehehhe', author: 'Mario', id: 1},
+        {title: 'My', body: 'hehe', author: 'Luigi', id: 2},
+        {title: 'My', body: 'he', author: 'Wario', id: 3}
+    ]);
 
     return (  
+        //Iterating over each row (item: blog) in map function
+        //Need a key attribute in case changes are made to the array 
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click</button>
+            
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+
+            ))}
         </div>
     );
-    //Note about invoking: We don't do handleClick() because the app invokes the call for us, but 
-    //we want the users to be able to do that, so we just leave a reference.
 }
  
 export default Home;
