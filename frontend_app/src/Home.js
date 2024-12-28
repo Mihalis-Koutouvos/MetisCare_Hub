af2833eb046b9fa77d9d3f48a7175ec4d450d13b
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2024-12-28 17:38:59 -0500

    Added in some functionality related to fetching data as a test for the AWS system I will have in place.

--- frontend_app/src/Home.js
+++ frontend_app/src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -15,6 +15,11 @@ const Home = () => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log('Use Effect Ran');
+        console.log(blogs);
+    });
 
     //Functions can be passed in as props
     return (  
