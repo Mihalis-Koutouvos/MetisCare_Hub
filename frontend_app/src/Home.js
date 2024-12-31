b5689dc62767df4bbbd756c1bb08a2f6bf1c42ba
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2024-12-31 15:59:54 -0500

    Removed some comments from the Home page

--- frontend_app/src/Home.js
+++ frontend_app/src/Home.js
@@ -2,13 +2,10 @@ import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    //Initial value is array of blogs in useState
-    //Each id needs to be unique
     const [blogs, setBlogs] = useState(null);
 
     
     useEffect(() => {
-        //Once we have the data back, .then runs
         fetch('http://localhost:3000/blogs')
             .then(res => {
               return res.json();
@@ -20,7 +17,6 @@ const Home = () => {
 
     
     return (  
-        //When need to use JavaScript, use {}
         <div className="home">
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
