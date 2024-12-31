0a8842f9e2c793657db5671be36455c8aba2a337
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2024-12-31 16:10:46 -0500

    Improved the fetch request for the json data and removed some comments and extra lines of code.

--- frontend_app/src/BlogList.js
+++ frontend_app/src/BlogList.js
@@ -1,9 +1,6 @@
-//Functions can be passed in as props
 const BlogList = ({blogs, title}) => {
-
     return ( 
       <div className="blog-list">
-        <h2>{title}</h2>
         {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>
                 <h2>{blog.title}</h2>
--- frontend_app/src/Home.js
+++ frontend_app/src/Home.js
@@ -3,21 +3,22 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
-    
     useEffect(() => {
         fetch('http://localhost:3000/blogs')
             .then(res => {
               return res.json();
             })
             .then(data => {
+                setIsLoading(false);
                 setBlogs(data);
             })
     }, []);
 
-    
     return (  
         <div className="home">
+            {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
