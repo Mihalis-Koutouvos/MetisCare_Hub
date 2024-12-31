3be917d42374faff9a1d8cc4f1adc95a1193a1bd
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2024-12-31 15:59:03 -0500

    Removed the delete functionality but began integrating json.

--- frontend_app/src/Home.js
+++ frontend_app/src/Home.js
@@ -4,32 +4,25 @@ import BlogList from './BlogList';
 const Home = () => {
     //Initial value is array of blogs in useState
     //Each id needs to be unique
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'hehehhe', author: 'Mario', id: 1},
-        {title: 'My', body: 'hehe', author: 'Luigi', id: 2},
-        {title: 'My', body: 'he', author: 'Mario', id: 3}
-    ]);
-
-    const [name, setName] = useState('Mario');
-
-    const handleDelete = (id) => {
-        //Returns new array based on original, so no mutation 
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     
     useEffect(() => {
-        console.log('Use Effect Ran');
-        console.log(name);
-    }, [name]);
+        //Once we have the data back, .then runs
+        fetch('http://localhost:3000/blogs')
+            .then(res => {
+              return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            })
+    }, []);
 
     
     return (  
+        //When need to use JavaScript, use {}
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <button onClick={() => setName('Luigi')}>Change Name</button>
-            <p>{name}</p>
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
 }
