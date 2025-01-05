f94b6e49e615ad4fa9995acffccfdef5f80d8333
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-01-05 16:58:09 -0500

    Added a bunch of styles to the form in the rooms page, which will be altered later as well

--- frontend_app/src/Rooms.js
+++ frontend_app/src/Rooms.js
@@ -2,6 +2,25 @@ const Rooms = () => {
     return (  
         <div className="rooms">
             <h2>Rooms</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input>
+                    type="text"
+                    required
+                </input>
+
+                <label>Blog Body:</label>
+                <textarea>
+                    required
+                </textarea>
+
+                <label>Blog Author:</label>
+                <select>
+                    <option value="Mario">Mario</option>
+                    <option value="Yoshi">Yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
