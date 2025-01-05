eac3a7a1ae8d679a166734d5b8a11ab10f5154d6
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-01-05 17:13:54 -0500

    Added the form functionality, but now I need to work on submitting the form

--- frontend_app/src/Rooms.js
+++ frontend_app/src/Rooms.js
@@ -1,4 +1,11 @@
+import {useState} from 'react';
+
 const Rooms = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mario');
+
+
     return (  
         <div className="rooms">
             <h2>Rooms</h2>
@@ -7,19 +14,29 @@ const Rooms = () => {
                 <input
                     type="text"
                     required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
 
                 <label>Blog Body:</label>
                 <textarea
                     required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
                 ></textarea>
 
                 <label>Blog Author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
                     <option value="Mario">Mario</option>
                     <option value="Yoshi">Yoshi</option>
                 </select>
                 <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
             </form>
         </div>
     );
