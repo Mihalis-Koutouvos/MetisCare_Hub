a7be39f5996b7a65ab52eaa7beef3364a86a6748
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-01-05 16:00:19 -0500

    Fixed the fetch error with an abort controller and removed the errors associated with useEffect in the home page.

--- frontend_app/src/useFetch.js
+++ frontend_app/src/useFetch.js
@@ -6,8 +6,12 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        //Use this to stop the fetch error when switching two fast between pages
+        const abortCont = new AbortController();
+
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, {signal: abortCont.signal})
             .then(res => {
                 if (!res.ok) {
                     throw Error('Could not fetch the data for that resource.');
@@ -20,10 +24,17 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err => {
-                setIsLoading(false);
-                setError(err.message);
+                if (err.name === 'AbortError') {
+                    console.log('Fetch aborted');
+                } else {
+                    setIsLoading(false);
+                    setError(err.message);
+                }
             })
         }, 1000);
+
+        return () => abortCont.abort();
+        //This aborts whatever fetch is caught with the controller
     }, [url]);
 
     return {data, isLoading, error};
